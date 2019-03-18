1a7b7ffc72ba740f2bc5e68034b9aaa13f33aa71
NagoW <48646485+NagoW@users.noreply.github.com>
2019-03-18 23:01:17 +0900

    update index.php
    Text48p

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
-    // Composerでインストールしたライブラリを一括読み込み
+    // Composerでインストールしたライブラリを一括読み込み,k
     require_once __DIR__ . '/vendor/autoload.php';
 
     // アクセストークンを使いCurlHTTPClientをインスタンス化
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('zUrfkarxLzMaGePYD54G/rtrH8gas9zOJSrpiTVaRB8sk/Dvg7/qxYt0WTtKRR095ozOBgzi1/hGc8sXUVeyRopNTjt59ztl3xbWmHu5loy7VO7F80GLNsfiJit8vJN6TOAPqVdTqc2TQMTnr3QYMAdB04t89/1O/w1cDnyilFU='));
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
     //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'Channel Secret']);
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('Channel Secret')]);
 
     // LINE Messaging APIがリクエストに付与した署名を取得
     $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
     //署名をチェックし、正当であればリクエストをパースし配列へ、不正であれば例外処理
     $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-
+　　
+　　//配列に格納された各イベントをループで処理
     foreach ($events as $event) {
+      //テキストを返信
+      $bot->replyText($event->getReplyToken(), 'TestMessage')
+    }
+
+    /* ================================後で加える箇所================================
+        foreach ($events as $event) {
         // メッセージを返信
         $response = $bot->replyMessage(
             $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
         );
-    }
+    }*/
 ?>
