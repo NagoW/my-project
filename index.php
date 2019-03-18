83a34fd5e2801d19597076cd22deb90171a2dc39
NagoW <48646485+NagoW@users.noreply.github.com>
2019-03-18 17:11:36 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
-<?php 
+<?php
+    // Composerでインストールしたライブラリを一括読み込み
+    require_once __DIR__ . '/vendor/autoload.php';
 
-// Composerでインストールしたライブラリを一括読み込み
-require_once __DIR__ . '/vendor/autoload.php';
+    // アクセストークンを使いCurlHTTPClientをインスタンス化
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Channel Access Token');
 
-// POSTメソッドで渡される値を取得、表示
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+    //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'Channel Secret']);
 
+    // LINE Messaging APIがリクエストに付与した署名を取得
+    $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+    //署名をチェックし、正当であればリクエストをパースし配列へ、不正であれば例外処理
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+    foreach ($events as $event) {
+        // メッセージを返信
+        $response = $bot->replyMessage(
+            $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
+        );
+    }
 ?>
