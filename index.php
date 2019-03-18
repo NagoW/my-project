e8959b778da06d5257196480037544f0e12d8cd7
NagoW <48646485+NagoW@users.noreply.github.com>
2019-03-18 23:28:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-<?php 
-  // Composerでインストールしたライブラリを一括読み込み
-  require_once __DIR__ . '/vendor/autoload.php';
-
-  // アクセストークンを使いCurlHTTPClientをインスタンス化
-  $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
-  //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-  $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('Channel Secret')]);
-
-  // LINE Messaging APIがリクエストに付与した署名を取得
-  $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-  //署名をチェックし、正当であればリクエストをパースし配列へ、不正であれば例外処理
-  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-　　
-  //配列に格納された各イベントをループで処理
-  foreach ($events as $event) {
-    //テキストを返信
-    $bot->replyText($event->getReplyToken(), 'TestMessage')
-  }
-  /* ================================後で加える箇所================================
-      foreach ($events as $event) {
-      // メッセージを返信
-      $response = $bot->replyMessage(
-          $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
-      );
-  }*/
-=======
 <?php
 
     // Composerでインストールしたライブラリを一括読み込み
@@ -58,5 +28,5 @@
             $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
         );
     }*/
->>>>>>> d6123080cce80b4a8de64ac94cc33e7cc2a58c4b
+    //d6123080cce80b4a8de64ac94cc33e7cc2a58c4b
 ?>
