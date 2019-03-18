205caa79ab9aeb70fcad4650968fb9f9659be83a
NagoW <48646485+NagoW@users.noreply.github.com>
2019-03-18 16:52:44 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php 
 
-echo "Hello World!";
-echo nl2br("\n"); //<br />タグが挿入される。
-echo "わかりづらすぎて草も生えない";
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+// POSTメソッドで渡される値を取得、表示
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 
 ?>
